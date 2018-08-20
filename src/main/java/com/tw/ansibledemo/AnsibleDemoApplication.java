fac2712c1f40751629c27d808e3808c5fc4c1404
Di Yuan <dyuan@thoughtworks.com>
2018-08-20 10:42:09 +0800

    first commit

---
+++ src/main/java/com/tw/ansibledemo/AnsibleDemoApplication.java
@@ -0,0 +1,15 @@
+package com.tw.ansibledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnsibleDemoApplication.class, args);
+        System.out.println("=======================");
+        System.out.println("Hello World!");
+        System.out.println("=======================");
+    }
+}
